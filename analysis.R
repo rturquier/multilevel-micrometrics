716e5233173c0ee3f8a6dbb2618ac0f806baaa1a
rturquier <remi.turquier@ensae.fr>
2022-11-02 14:19:52 +0100

    👀 Open a data file

--- analysis.R
+++ analysis.R
@@ -9,3 +9,6 @@ datasets_path  <- "ICPSR_30263"
 
 
 ####=====================  3. Main code  ======================####
+first_df <- read_dta(
+  file.path(datasets_path, "DS0001/30263-0001-Data.dta")
+)
