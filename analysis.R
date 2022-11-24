bef9b1d85ab6464ccbc12dd438cf8c6fbe29e60d
rturquier <remi.turquier@ensae.fr>
2022-11-24 16:00:19 +0100

    🤏 Select relevant variables

--- analysis.R
+++ analysis.R
@@ -12,3 +12,20 @@ datasets_path  <- "ICPSR_30263"
 first_df <- read_dta(
   file.path(datasets_path, "DS0001/30263-0001-Data.dta")
 )
+
+relevant_variables <- c(
+  "CASENUM",
+  "COHORT",
+  paste0(LETTERS[1:6], "MTHTCH"),
+  paste0(LETTERS[7:12], "MTHTCH1"),
+  paste0(LETTERS[7:12], "AMTH2B"),
+  paste0(LETTERS[1:12][c(T, F)], "MTHIMP"),
+  paste0(LETTERS[1:12][c(T, F)], "MTHIMPF"),
+  paste0(LETTERS[1:12][c(T, F)], "MTHIRT"),
+  paste0(LETTERS[1:6], "BMTHJ"),
+  paste0(LETTERS[7:12], "AMTH1J"),
+  paste0(LETTERS[7:12], "AMTH2J")
+)
+
+main_df <- first_df %>%
+  select(all_of(relevant_variables))
