14033ceafe7ef4701c697969073f5055169af11b
Rémi Turquier <57546969+rturquier@users.noreply.github.com>
2022-12-11 16:57:15 +0100

    🧑🏻‍🍳 Prepare data (#3)
    Select, filter, reshape and format the LSAY dataset
    
    * 🤏 Select relevant variables
    
    * ⤵️ Pivot tibble to longer form to get 1 obs / row
    
    That was issue #2  🎉
    `pivot_longer` is a bit hard to understand, but now that I do, it's
    super powerful ⚡
    
    * 🤷 Replace missing value codes with NA
    
    Missing values are coded with numbers from -99 to -95 in the survey
    This commit replaces these values with R missing values (NA)
    
    * 2️⃣  Filter for cohort 2
    
    * 🥂 Coalesce homework and coalesce teacher id
    
    * 🗓️  Create year variable from semester letter
    
    * 🏷️ Rename columns
    
    * 🏖️  Summarise at student-year level
    
    In some cases, there's a different teacher for
    the two semesters of a same year. In that case,
    the current solution is to just take the first
    one.
    
    * 🍂 Consider first semester only
    
    Because math tests happen in Fall
    
    * 🥡 Put data preparation work in a function

--- analysis.R
+++ analysis.R
@@ -4,11 +4,54 @@ library(tidyverse)
 
 datasets_path  <- "ICPSR_30263"
 
-
 ####=====================  2. Functions  ======================####
+convert_semester_to_year <- function(semester){
+  first_year <- 1987
+  positions_in_alphabet <- match(semester, LETTERS)
+  years_from_start <- (positions_in_alphabet - 1) %/% 2
+  years <- first_year + years_from_start
+  return(years)
+}
 
+prepare <- function(LSAY_data){
+  # Select, filter, reshape and format the LSAY dataset
+  
+  relevant_variables <- c(
+    "CASENUM",
+    "COHORT",
+    paste0(LETTERS[1:6], "MTHTCH"),
+    paste0(LETTERS[7:12], "MTHTCH1"),
+    paste0(LETTERS[7:12], "AMTH2B"),
+    paste0(LETTERS[1:12][c(T, F)], "MTHIMP"),
+    paste0(LETTERS[1:12][c(T, F)], "MTHIMPF"),
+    paste0(LETTERS[1:12][c(T, F)], "MTHIRT"),
+    paste0(LETTERS[1:6], "BMTHJ"),
+    paste0(LETTERS[7:12], "AMTH1J"),
+    paste0(LETTERS[7:12], "AMTH2J")
+  )
+  
+  missing_value_codes <- -99:-95
+  
+  LSAY_data %>%
+    select(all_of(relevant_variables)) %>%
+    filter(COHORT == 2) %>%
+    pivot_longer(cols = contains("MTH"),
+                 names_to = c("semester", ".value"),
+                 names_pattern = "([A-L])[AB]?(MTH.+)") %>%
+    mutate(across(everything(),
+                  ~ ifelse(. %in% missing_value_codes, NA, .))) %>%
+    mutate(teacher  = coalesce(MTHTCH, MTHTCH1, MTH2B),
+           homework = coalesce(MTHJ, MTH1J, MTH2J),
+           year     = semester %>% convert_semester_to_year()) %>%
+    rename(grade    = MTHIMP,
+           student  = CASENUM) %>%
+    group_by(student, year) %>%
+    summarise(across(c(teacher, grade, homework), first))
+}
 
 ####=====================  3. Main code  ======================####
-first_df <- read_dta(
+LSAY_data <- read_dta(
   file.path(datasets_path, "DS0001/30263-0001-Data.dta")
 )
+
+main_df <- prepare(LSAY_data)
