498ee990dd3dd1f48f1701b90028c594b95ef386
eveansari <62470862+eveansari@users.noreply.github.com>
2023-01-09 15:42:58 +0100

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -2,6 +2,7 @@
 library(haven)
 library(tidyverse)
 
+#install.packages("Matrix")
 datasets_path  <- "ICPSR_30263"
 
 ####=====================  2. Functions  ======================####
@@ -55,3 +56,75 @@ LSAY_data <- read_dta(
 )
 
 main_df <- prepare(LSAY_data)
+<<<<<<< Updated upstream
+=======
+
+# Scatter plot
+main_df %>%
+  ggplot(aes(x = homework_teacher, y = grade)) + 
+  geom_smooth(method = "lm", color="#FF3BA05D", linewidth = 0.5, se = F) +
+  geom_jitter(alpha = 0.07) +
+  labs(x = "Weekly hours of homework given", y = "") +
+  ggtitle("Math test grade out of 100") +
+  scale_x_continuous(minor_breaks = c(1:4, 6:9, 12)) +
+  scale_y_continuous(breaks = 0:4 * 25, limits = c(0, 100)) +
+  theme_minimal() +
+  theme(plot.title = element_text(size = 11, hjust = -0.08, vjust = -2))
+
+
+# ---- Regressions ----
+ols <- lm(grade ~ homework_teacher, main_df)
+
+student_dummies <- lm(grade ~ homework_teacher + student, main_df)
+
+random_effects <- plm::plm(grade ~ homework_teacher,
+                           data = main_df,
+                           index = c("student", "year"),
+                           model = "random")
+
+within_student <- plm::plm(grade ~ homework_teacher,
+                           data = main_df,
+                           index = c("student", "year"),
+                           model = "within")
+
+first_difference_student <- plm::plm(grade ~ homework_teacher,
+                                     data = main_df,
+                                     index = c("student", "year"),
+                                     model = "fd")
+
+within_teacher <- plm::plm(grade ~ homework_teacher,
+                           data = main_df,
+                           index = c("teacher", "year"),
+                           model = "within")
+
+within_students_with_teacher_dummies <- plm::plm(
+  grade ~ homework_teacher + teacher,
+  data = main_df,
+  index = c("student", "year"),
+  model = "within"
+)
+
+zig_zag <- lfe::felm(grade ~ homework_teacher | student + teacher, main_df)
+
+# ----- Tests ------
+
+##  ------ Homoskedasticity at the student level ------
+
+#Breusch Pagan between pooled and RE
+
+BP_student <- plmtest(grade ~ homework_teacher, data=main_df, type="bp")
+BP_student
+
+##  Are estimates statiscally different ?
+
+#Hausman between RE and FE (single level panel)
+
+Haus_re_fe_stud <- phtest(random_effects, within_student)
+Haus_re_fe_stud
+
+#Hausman between mix and FE
+Haus_mix_fe <- phtest(within_student, within_students_with_teacher_dummies)
+Haus_mix_fe
+
+
+>>>>>>> Stashed changes
